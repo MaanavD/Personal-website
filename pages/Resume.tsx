255ecd8a14369c605d5d651036d432652bdd8566
Maanav Dalal <dalalm1@mcmaster.ca>
2022-05-27 18:59:54 +0200

    Resume page with inline working.

--- pages/Resume.tsx
+++ pages/Resume.tsx
@@ -1,50 +1,10 @@
 import React from 'react'
-import { Center, Text, List, ListItem, ListIcon, useColorModeValue } from '@chakra-ui/react'
-import { GoChevronRight } from "react-icons/go"
-
-let sections = ['Education', 'Skills', 'Experience', 'Projects']
-let sectionContent = [{}, {}, {}, {}]
-const style = {
-    "parent": {
-        "display": "grid",
-        "gridTemplateColumns": "repeat(3, 1fr)",
-        "gridTemplateRows": "repeat(2, 1fr)",
-        "gridColumnGap": "0px",
-        "gridRowGap": "0px"
-    },
-    "div1": {
-        "gridArea": "1 / 1 / 2 / 2"
-    },
-    "div2": {
-        "gridArea": "2 / 1 / 3 / 2"
-    },
-    "div3": {
-        "gridArea": "1 / 2 / 3 / 3"
-    },
-    "div4": {
-        "gridArea": "1 / 3 / 3 / 4"
-    }
-}
+import { calc, Container } from '@chakra-ui/react'
 const Resume = () => {
     return (
-        <>
-            <div style={style.parent}>
-                <div style={style.div1}>
-                    <Text fontSize='6xl'>{sections[0]}</Text>
-                    <Text fontSize='xl'>McMaster University - B.ENG. SOFTWARE ENGINEERING</Text>
-                    <List spacing={3}>
-                        <ListItem>
-                            <ListIcon as={GoChevronRight} color={useColorModeValue("blue.300", "blue.500")} />
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit
-                        </ListItem>
-
-                    </List>
-                </div>
-                <div style={style.div2}>{sections[1]}</div>
-                <div style={style.div3}>{sections[2]}</div>
-                <div style={style.div4}>{sections[3]}</div>
-            </div>
-        </>
+        <div style={{height: `calc(100vh - 65px)`}}>
+            <iframe height="100%" width="100%" src="https://drive.google.com/file/d/1_5GPPM1D1QHuYY-QlpikEZnQSkIDKP7i/preview#view=FitH"></iframe>
+        </div>
     )
 }
 
--- pages/components/Achievements.tsx
+++ pages/components/Achievements.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Text } from "@chakra-ui/react";
+import { Text, Link, useColorModeValue} from "@chakra-ui/react";
 
 const Achievements = () => {
     return (
         <>
             <Text fontSize="3xl" align='center'>Achievements & Awards</Text>
-            <Text align='center'>Here's where anything I'm proud of goes.</Text>
+            <Text align='center'>Here's where anything I'm proud of goes. See more on <a color={useColorModeValue("blue.500", "blue.200")} href='https://www.linkedin.com/in/maanavdalal' isExternal> LinkedIn</a>.</Text>
             <br/>
         </>
     )
