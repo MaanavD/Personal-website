2599cd30d8a8ed5e8548dd88406a8fe3d3822c5d
Maanav Dalal <dalalm1@mcmaster.ca>
2022-05-27 19:48:52 +0200

    Completed contact page.

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Text, Badge, Flex, Box, Stack, Button, ButtonGroup, Heading, chakra } from "@chakra-ui/react";
+import { Text, Badge, Flex, Box, Stack, Button, ButtonGroup, Heading, chakra, HStack, Container, Grid } from "@chakra-ui/react";
 import NextImage from 'next/image'
+import { FaEnvelope, FaGithub, FaInstagram, FaLinkedinIn, FaSpotify } from 'react-icons/fa';
 
 const Image = chakra(NextImage, {
     shouldForwardProp: (prop) => ['width', 'height', 'src', 'alt'].includes(prop)
@@ -9,9 +10,39 @@ const Image = chakra(NextImage, {
 export default function Projects() {
     return (
         <>
-        <br/>
-            <Text fontSize="3xl" align='center'>Drop me a line.</Text>
-            <br/>
+            <br />
+            <Text fontSize="3xl" align='center'>Connect with me at:</Text>
+            <br />
+            <Container maxW="container.sm" padding="10">
+                <Grid gap={6} templateColumns='auto' >
+
+                    <a target="_blank" href="https://github.com/MaanavD/" rel="noopener noreferrer">
+                        <Button size='lg' width="100%" colorScheme='gray' leftIcon={<FaGithub />}>
+                            Github
+                        </Button>
+                    </a>
+                    <a target="_blank" href="https://www.linkedin.com/in/maanavdalal/" rel="noopener noreferrer">
+                        <Button size='lg' width="100%" colorScheme='linkedin' leftIcon={<FaLinkedinIn />}>
+                            LinkedIn
+                        </Button>
+                    </a>
+                    <a target="_blank" href="mailto:maanavdalal@gmail.com?Subject=Let%27s%20Talk" rel="noopener noreferrer">
+                        <Button size='lg' width="100%" colorScheme='red' leftIcon={<FaEnvelope />}>
+                            Email
+                        </Button>
+                    </a>
+                    <a target="_blank" href="https://www.instagram.com/maanav_/" rel="noopener noreferrer">
+                        <Button size='lg' width="100%" colorScheme='pink' leftIcon={<FaInstagram />}>
+                            Instagram
+                        </Button>
+                    </a>
+                    <a target="_blank" href="https://open.spotify.com/user/ultimated100" rel="noopener noreferrer">
+                        <Button size='lg' width="100%" colorScheme='green' leftIcon={<FaSpotify />}>
+                            Spotify
+                        </Button>
+                    </a>
+                </Grid>
+            </Container>
         </>
     )
 }
