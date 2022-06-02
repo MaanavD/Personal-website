ac48bf59f2a780153808fe1b5a521ed940ebcf80
Maanav Dalal <dalalm1@mcmaster.ca>
2022-06-02 17:24:43 +0200

    Added sitemap for SEO.

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://maanavdalal.com',
+  generateRobotsTxt: true, // (optional)
+  // REST CODE READ DOCS  ...
+  }
